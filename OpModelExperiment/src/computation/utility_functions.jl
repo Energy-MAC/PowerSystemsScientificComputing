c5fc23818b6cc5289626d02a9c76d3d9e8256bd5
Jose Daniel Lara <jdlara@berkeley.edu>
2019-09-23 16:16:40 -0700

    add more utility functions

--- OpModelExperiment/src/computation/utility_functions.jl
+++ OpModelExperiment/src/computation/utility_functions.jl
@@ -56,13 +56,67 @@ function solve_JuMPmodel!(JuMPmodel::JuMP.Model; kwargs...)
 
 end
 
+function make_initial_conditions_from_data(sys::PowerSystems.System)
+    inital_conditions = Dict{String, Any}()
+    for gen in PowerSystems.get_components(PowerSystems.ThermalStandard, sys)
+        ini_g = Dict{Symbol, Float64}()
+        name = PowerSystems.get_name(gen)
+        ini_g[:power_output_t0] =  PowerSystems.get_activepower(gen)
+        activepowerlimits = PowerSystems.get_tech(gen) |> PowerSystems.get_activepowerlimits
+        ini_g[:min_power] = activepowerlimits.min
+        ini_g[:unit_on_t0] = 1.0*(ini_g[:power_output_t0] > 0)
+        ini_g[:time_down_t0] =  999.0*(1.0 - (ini_g[:power_output_t0] > 0))
+        ini_g[:time_up_t0] =  999.0*(ini_g[:power_output_t0] > 0)
+        inital_conditions[name] = ini_g
+    end
+    return inital_conditions
+end
+
 function update_commitment_status!(ed_model, uc_model, t)
 
-    status_solutions = JuMP.value.(uc_model.obj_dict[:ug])[:,1]
+    status_solutions = JuMP.value.(uc_model.obj_dict[:ug])[:,t]
     for (ix ,v) in enumerate(status_solutions)
         name = axes(status_solutions)[1][ix]
         JuMP.fix.(ed_model.obj_dict[:ug][name, :], v; force = true)
     end
-
     return
+end
+
+function update_initial_conditions!(ic, uc_m, ed_m, t)
+
+    pg = ed_m[:pg]
+    ug = uc_m[:ug]
+
+    for (k, v) in ic
+        #@assert ug[k, t] == 1.0*(JuMP.value(pg[k, t]) > eps())
+        v[:power_output_t0] = JuMP.value(pg[k, end]) + JuMP.value(ug[k, t])*v[:min_power]
+        # If the unit was on, add to the count
+        if v[:unit_on_t0] > 0
+            @show k, v[:unit_on_t0], JuMP.value(ug[k, t])
+            if v[:unit_on_t0] == JuMP.value(ug[k, t])
+                v[:time_up_t0] += 1.0
+            elseif v[:unit_on_t0] != JuMP.value(ug[k, t])
+                v[:time_up_t0] = 0.0
+                v[:time_down_t0] = 1.0
+            end
+            v[:unit_on_t0] = JuMP.value(ug[k, t])
+            @show k, v
+            continue
+        end
+
+        # If the unit was off, add to the count
+        if v[:unit_on_t0] < 1
+            @show k, v[:unit_on_t0], JuMP.value(ug[k, t])
+            if v[:unit_on_t0] == JuMP.value(ug[k, t])
+                v[:time_down_t0] += 1.0
+            elseif v[:unit_on_t0] != JuMP.value(ug[k, t])
+                v[:time_up_t0] = 1.0
+                v[:time_down_t0] = 0.0
+            end
+            v[:unit_on_t0] = JuMP.value(ug[k, t])
+            @show k, v
+            continue
+        end
+    end
+
 end
