2a7cc2f5b41d662d93be150cd7212d06a86be2f3
Jose Daniel Lara <jdlara@berkeley.edu>
2019-09-25 22:59:01 -0700

    update utility functions

--- OpModelExperiment/src/computation/utility_functions.jl
+++ OpModelExperiment/src/computation/utility_functions.jl
@@ -1,23 +1,6 @@
-function prepare_workspace!(base_name::String, folder::String)
-
-    !isdir(folder) && error("Specified folder is not valid")
-
-    cd(folder)
-    global_path = joinpath(folder)
-    isdir(global_path) && mkpath(global_path)
-    simulation_path = joinpath(global_path, "$(round(Dates.now(),Dates.Minute))-$(base_name)")
-    raw_ouput = joinpath(simulation_path)
-    mkpath(raw_ouput)
-
-    return raw_ouput
-
-end
-
-function solve_JuMPmodel!(JuMPmodel::JuMP.Model; kwargs...)
+function solve_JuMPmodel!(JuMPmodel::JuMP.Model, save_path::String; kwargs...)
 
     timed_log = Dict{Symbol, Any}()
-
-    save_path = get(kwargs, :save_path, nothing)
 
     if JuMPmodel.moi_backend.state == MOIU.NO_OPTIMIZER
 
@@ -40,17 +23,16 @@ function solve_JuMPmodel!(JuMPmodel::JuMP.Model; kwargs...)
     #creating the results to print to memory
     vars_result = get_model_result(JuMPmodel)
     optimizer_log = get_optimizer_log(JuMPmodel)
-    time_stamp = get_time_stamp(JuMPmodel)
     obj_value = Dict(:OBJECTIVE_FUNCTION => JuMP.objective_value(JuMPmodel))
     merge!(optimizer_log, timed_log)
 
     #results to be printed to memory
     results = Dict(:vars => vars_result,
                    :obj_value => obj_value,
-                   :optimizer_log => optimizer_log,
-                   :time_stamp => time_stamp)
+                   :optimizer_log => optimizer_log)
 
-    !isnothing(save_path) && write_model_results(results, save_path)
+    @show save_path
+    write_model_results(results, save_path)
 
     return
 
