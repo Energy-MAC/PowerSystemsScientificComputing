23205d0bd37cae7f850bd3bc13544a0c4e6991de
Jose Daniel Lara <jdlara@berkeley.edu>
2019-09-25 22:57:56 -0700

    add experiment utilties

---
+++ OpModelExperiment/src/experiment_utilities.jl
@@ -0,0 +1,19 @@
+function remove_char(s::String, char::String)
+    return replace(s, Regex("[$char]") => "")
+end
+
+function prepare_workspace!(base_name::String, folder::String)
+
+    !isdir(folder) && error("Specified folder is not valid")
+
+    cd(folder)
+    global_path = joinpath(folder)
+    isdir(global_path) && mkpath(global_path)
+    _sim_path =  remove_char("$(round(Dates.now(),Dates.Minute))-$(base_name)", ":")
+    simulation_path = joinpath(global_path,)
+    raw_ouput = joinpath(simulation_path)
+    mkpath(raw_ouput)
+
+    return raw_ouput
+
+end
