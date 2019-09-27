73d82a13ee8b6b5be854eaf8bed757238940ffb8
Jose Daniel Lara <jdlara@berkeley.edu>
2019-09-26 21:43:57 -0700

    add get results for 3 dim arrays

--- OpModelExperiment/src/results/get_results.jl
+++ OpModelExperiment/src/results/get_results.jl
@@ -1,18 +1,41 @@
-function _result_dataframe_vars(variable::JuMP.Containers.DenseAxisArray)
+function _result_dataframe_vars(variable::JuMP.Containers.DenseAxisArray, get_function)
 
-    result = Array{Float64, length(variable.axes)}(undef, length(variable.axes[2]), length(variable.axes[1]))
-    names = Array{Symbol, 1}(undef, length(variable.axes[1]))
+    if length(axes(variable)) < 3
 
-    for t in variable.axes[2], (ix, name) in enumerate(variable.axes[1])
-        result[t, ix] = JuMP.value(variable[name, t])
-        names[ix] = Symbol(name)
+        result = Array{Float64, 2}(undef, length(last(variable.axes)), length(first(variable.axes)))
+        names = Array{Symbol, 1}(undef, length(variable.axes[1]))
+
+        for t in variable.axes[2], (ix, name) in enumerate(variable.axes[1])
+            result[t, ix] = get_function(variable[name, t])
+            names[ix] = Symbol(name)
+        end
+
+        return DataFrames.DataFrame(result, names)
+
+    elseif length(axes(variable)) == 3
+        extra_dims = sum(length(axes(variable)[2:end-1]))
+        extra_vars = [Symbol("S$(s)") for s in 1:extra_dims]
+        result_df = DataFrames.DataFrame()
+        names = vcat(extra_vars, Symbol.(axes(variable)[1]))
+
+        for i in variable.axes[2]
+            third_dim = collect(fill(i,size(variable)[end]))
+            result = Array{Float64 ,2}(undef, length(last(variable.axes)),
+                                              length(first(variable.axes)))
+            for t in last(variable.axes), (ix, name) in enumerate(first(variable.axes))
+                result[t, ix] = get_function(variable[name, i, t])
+            end
+            res = DataFrames.DataFrame(hcat(third_dim, result))
+            result_df = vcat(result_df, res)
+        end
+
+        return DataFrames.names!(result_df, names)
+
     end
-
-    return DataFrames.DataFrame(result, names)
 
 end
 
-function _result_dataframe_vars(variable::JuMP.Containers.SparseAxisArray)
+function _result_dataframe_vars(variable::JuMP.Containers.SparseAxisArray, get_function)
     return Vector()
 end
 
@@ -21,8 +44,11 @@ function get_model_result(m::JuMP.Model)
     results_dict = Dict{Symbol, Any}()
 
     for (k, v) in m.obj_dict
-        results_dict[k] = _result_dataframe_vars(v)
+        results_dict[k] = _result_dataframe_vars(v, JuMP.value)
     end
+
+    results_dict[:pw_ub] = _result_dataframe_vars(m.obj_dict[:pw], JuMP.upper_bound)
+    results_dict[:il_ub] = _result_dataframe_vars(m.obj_dict[:pl], JuMP.upper_bound)
 
     return results_dict
 
